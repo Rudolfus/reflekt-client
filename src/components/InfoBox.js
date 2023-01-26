2c8f77efbeb60e50e652a568b7adb821f269e3dc
Rudolf Teibtner <rudolfteibtner@mailbox.org>
2023-01-26 15:28:31 +0100

    add first draft homepage with components

--- src/components/InfoBox.js
+++ src/components/InfoBox.js
@@ -35,6 +35,6 @@ const InfoBox = () => {
       </div>
     </div>
   );
-}
+};
 
 export default InfoBox
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,5 +1,18 @@
+import InfoBox from "../components/InfoBox";
+import { SetOfQuestions } from "../components/SetOfQuestions";
+import { QuestionCarousel } from "../components/QuestionCarousel";
+
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <div>
+      {/* landing page */}
+      <InfoBox />
+      <QuestionCarousel />
+      <SetOfQuestions />
+      {/* info about science page */}
+      <button>Try it now</button>
+    </div>
+  );
 };
 
 export default HomePage;
