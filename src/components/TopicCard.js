36c61f6278ec9e532d2755a294f542492ed1d839
Rudolf Teibtner <rudolfteibtner@mailbox.org>
2023-01-31 14:50:47 +0100

    fix minor bug in TopicCard

--- src/components/TopicCard.js
+++ src/components/TopicCard.js
@@ -4,41 +4,50 @@ const TopicCard = () => {
   return (
     <div>
       <section>
-        <NavLink to="/question/self-care">self-care
+        <NavLink to="/question/self-care">
+          self-care
           {/* <button className="button accept-btn" onClick="addAnimation2()">
             self-care
           </button> */}
         </NavLink>
 
-        <NavLink to="/question/smoking">smoking
+        <NavLink to="/question/smoking">
+          smoking
           {/* <button className="button accept-btn" onClick="addAnimation2()">
             smoking
           </button> */}
         </NavLink>
 
-        <NavLink to="/question/job">job
+        <NavLink to="/question/job">
+          job
           {/* <button className="button accept-btn" onClick="addAnimation2()">
             job
           </button> */}
         </NavLink>
 
-        <NavLink to="/question/motivation"> motivation
+        <NavLink to="/question/motivation">
+          {" "}
+          motivation
           {/* <button className="button accept-btn" onClick="addAnimation2()">
             motivation
           </button> */}
         </NavLink>
 
-        <NavLink to="/question/eating"> eating
+        <NavLink to="/question/eating">
+          {" "}
+          eating
           {/* <button className="button accept-btn" onClick="addAnimation2()">
             eating
           </button> */}
         </NavLink>
 
-        <NavLink to="/question/relationships"> relationships
+        <NavLink to="/question/relationships">
+          {" "}
+          relationships
           {/* <button className="button accept-btn" onClick="addAnimation2()">
             relationships
-          </button>
-        </NavLink> */}
+          </button> */}
+        </NavLink>
       </section>
     </div>
   );
